9884becc5e3e173d60e0964f338820f29bd00635
rd-zf-zfy <49666819+rd-zf-zfy@users.noreply.github.com>
2019-04-16 11:24:48 +0800

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,6 +1,11 @@
 public class test1 {
   public static void main(String args[]){
-  System.out.println("Hello World！")；
-  System.out.println("大家好！")；
+  new Thead(new Connet()).start();
+  }
+}
+class Connet extends Runnable{
+  public void run(){
+    System.out.println("Hello World！")；
+    System.out.println("大家好！")；
   }
 }
